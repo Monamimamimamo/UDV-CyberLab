a186d9792453befa2db55cfe83c3fe8388fa0d95
Ivan Rybin <rubin1a@bk.ru>
2025-10-26 17:50:32 +0500

    adds.

--- Api/NewsService/Domain/Entities/NewsCard.cs
+++ Api/NewsService/Domain/Entities/NewsCard.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Cards;
 
 namespace Domain.Entities
 {
-    internal class NewsCard
+    public record NewsCard : CardEntity
     {
+        public string? URL { get; set; }
     }
 }
--- Api/NewsService/Infrastucture/NewsDbContext.cs
+++ Api/NewsService/Infrastucture/NewsDbContext.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Cards;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastucture
 {
-    internal class NewsDbContext
+    public class NewsDbContext : DbContext
     {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<NewsCard> Cards { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Card)
+                .WithMany()
+                .HasForeignKey(c => c.CardId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Rating>()
+                .HasOne(r => r.Card)
+                .WithMany()
+                .HasForeignKey(r => r.CardId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Make ProjectId and UserId a unique constraint for ratings
+            modelBuilder.Entity<Rating>()
+                .HasIndex(r => new { r.CardId, r.UserId })
+                .IsUnique();
+        }
+
     }
 }
